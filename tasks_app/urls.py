a6752e49e1645b493218dad076731a982a165dff
Hammed Olugbenga <koredeearth@gmail.com>
2025-04-19 07:45:01 +0100

    ...

--- tasks_app/urls.py
+++ tasks_app/urls.py
@@ -4,5 +4,5 @@ from .views import ProcessTaskView, TaskStatusView, public_health_check
 urlpatterns = [
     path('process/', ProcessTaskView.as_view(), name='process-task'),
     path('status/<str:task_id>/', TaskStatusView.as_view(), name='task-status'),
-     path('public-health-check/', public_health_check, name='public-health-check'),
+    path('public-health-check/', public_health_check, name='public-health-check'),
 ]
